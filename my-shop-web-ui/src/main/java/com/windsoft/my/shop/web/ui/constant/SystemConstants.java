409fecc7df9986dd20f5a320b042aeed6173b3f9
leaf <leaf@foxmail.com>
2019-11-23 12:57:32 +0800

    UI拦截器实现

---
+++ my-shop-web-ui/src/main/java/com/windsoft/my/shop/web/ui/constant/SystemConstants.java
@@ -0,0 +1,13 @@
+package com.windsoft.my.shop.web.ui.constant;
+
+/**
+ * @ClassName SystemConstants
+ * @Description
+ * @Author RicostTempest
+ * @Date 2019/11/23 12:23
+ * @Version V1.0
+ **/
+
+public class SystemConstants {
+    public static final String SESSION_USER_KEY = "tbUser";
+}
